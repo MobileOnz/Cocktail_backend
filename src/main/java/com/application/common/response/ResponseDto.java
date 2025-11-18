69df4a1a55e7aa09c1e1d13010c2881c68f270a4
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-11-19 02:02:20 +0900

    [#9] docs: schema 추가

--- src/main/java/com/application/common/response/ResponseDto.java
+++ src/main/java/com/application/common/response/ResponseDto.java
@@ -1,13 +1,20 @@
 package com.application.common.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
+@RequiredArgsConstructor
+@Schema(description = "공통 응답 객체")
 public class ResponseDto<T> {
-    private final Integer code; // -1 :실패 , 1: 성공
-    private final String msg;
-    private final T data; // json data
 
+    @Schema(description = "응답 코드 (1: 성공, -1: 실패, -2: 토큰만료 등)", example = "1")
+    private final Integer code; // -1 :실패 , 1: 성공
+
+    @Schema(description = "응답 메시지", example = "요청이 성공하였습니다.")
+    private final String msg;
+
+    @Schema(description = "응답 데이터 (결과가 없으면 null)")
+    private final T data; // json data
 }
