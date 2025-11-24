903f02f2ed8d2db7bd5f79968f823873de2e6b4c
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-11-24 19:59:01 +0900

    [#11] fix: cocktail entity postgre로 수정

--- src/main/java/com/application/domain/cocktail/entity/Cocktail.java
+++ src/main/java/com/application/domain/cocktail/entity/Cocktail.java
@@ -5,48 +5,51 @@ import com.application.domain.cocktail.enums.AbvLevel;
 import com.application.domain.cocktail.enums.Season;
 import com.application.domain.cocktail.enums.TasteLevel;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 import org.hibernate.annotations.Comment;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 @Getter
-@Entity(name = "cocktail")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "cocktail")
+
 public class Cocktail {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
-    @Comment("Cocktail KR Name")
+    @Column(name = "cocktail_kr_name", nullable = false)
     private String cocktailKR;
-    @Column
-    @Comment("Cocktail EN Name")
+
+    @Column(name = "cocktail_en_name", nullable = false)
     private String cocktailEN;
 
-    @Column
-    @Comment("max Alcohol")
+    @Column(name = "max_alcohol")
     private Integer maxAlcohol;
-    @Column
-    @Comment("min Alcohol")
-    private Integer minAlcohol;
-    @Column(columnDefinition = "TEXT")
-    @Comment("origin text")
-    private String originText;
-    @Column(length = 1000)
-    @Comment("image URL")
-    private String imageUrl;
 
+    @Column(name = "min_alcohol")
+    private Integer minAlcohol;
+
+    @Column(name = "origin_text", columnDefinition = "TEXT")
+    private String originText;
+
+    @Column(name = "image_url", length = 1000)
+    private String imageUrl;
 
     @Convert(converter = AbvLevelConverter.class)
     @Comment("도수 Level : enum 관리")
     private AbvLevel abvBand; // WEEK, NORMAL, STRONG
+
     @Convert(converter= TasteLevelConverter.class)
     @Comment("taste Level : enum 관리")
     private TasteLevel tasteLevel; // BEGINNER, INTERMEDIATE, ADVANCED
+
     @ElementCollection(targetClass = Season.class)
     @CollectionTable(
             name = "cocktail_season",
@@ -55,6 +58,7 @@ public class Cocktail {
     @Enumerated(EnumType.STRING) // Enum 이름으로 저장
     @Column(name = "season")
     private List<Season> seasons = new ArrayList<>();
+
     @OneToMany(mappedBy = "cocktail", cascade = CascadeType.ALL, orphanRemoval = true)
     @Comment("ingredients")
     private List<Ingredient> ingredients = new ArrayList<>();
@@ -63,30 +67,9 @@ public class Cocktail {
     @OneToMany(mappedBy = "cocktail", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CocktailTag> tags = new ArrayList<>();
 
-
-
-    public Cocktail(){}
-
-    @Builder
-    public Cocktail(String cocktailEN, String cocktailKR
-                    , AbvLevel abvBand, Integer maxAlcohol, Integer minAlcohol
-                    , TasteLevel tasteLevel, String originText, String imageUrl
-                    , List<Season> seasons, List<Ingredient> ingredients
-                    , List<CocktailTag> tags
-                    )
-    {
-        this.cocktailEN  = cocktailEN;
-        this.cocktailKR  = cocktailKR;
-        this.abvBand     = abvBand;
-        this.maxAlcohol  = maxAlcohol;
-        this.minAlcohol  = minAlcohol;
-        this.tasteLevel  = tasteLevel;
-        this.originText  = originText;
-        this.imageUrl    = imageUrl;
-        this.seasons     = seasons;
-        this.ingredients = ingredients;
-        this.tags       = tags;
-    }
+    /**
+     * 비즈니스 로직 (setter 대신)
+     */
 
     public void update(String cocktailEN, String cocktailKR
             , AbvLevel abvBand, Integer maxAlcohol, Integer minAlcohol
@@ -112,5 +95,29 @@ public class Cocktail {
         CocktailTag cocktailTag = new CocktailTag(this, tag);
         tags.add(cocktailTag);
     }
+
+//    public Cocktail(){}
+//
+//    @Builder
+//    public Cocktail(String cocktailEN, String cocktailKR
+//                    , AbvLevel abvBand, Integer maxAlcohol, Integer minAlcohol
+//                    , TasteLevel tasteLevel, String originText, String imageUrl
+//                    , List<Season> seasons, List<Ingredient> ingredients
+//                    , List<CocktailTag> tags
+//                    )
+//    {
+//        this.cocktailEN  = cocktailEN;
+//        this.cocktailKR  = cocktailKR;
+//        this.abvBand     = abvBand;
+//        this.maxAlcohol  = maxAlcohol;
+//        this.minAlcohol  = minAlcohol;
+//        this.tasteLevel  = tasteLevel;
+//        this.originText  = originText;
+//        this.imageUrl    = imageUrl;
+//        this.seasons     = seasons;
+//        this.ingredients = ingredients;
+//        this.tags       = tags;
+//    }
+//
 
 }
