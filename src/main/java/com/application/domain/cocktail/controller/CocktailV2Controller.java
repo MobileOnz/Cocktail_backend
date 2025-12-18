a970a438c9e76fc1812f8889c7d731517953c552
jsgjsg <jsgcys0815@gmail.com>
2025-12-18 23:25:30 +0900

    [#13] fix: 충돌 코드 merge

--- src/main/java/com/application/domain/cocktail/controller/CocktailV2Controller.java
+++ src/main/java/com/application/domain/cocktail/controller/CocktailV2Controller.java
@@ -325,18 +325,6 @@ public class CocktailV2Controller implements CocktailV2ControllerDocs{
 
     @Getter
     @Setter
-    public static class CocktailNameDto {
-        private int id;
-        private String name;
-
-        public CocktailNameDto(int id, String name) {
-            this.id = id;
-            this.name = name;
-        }
-    }
-
-    @Getter
-    @Setter
     public static class CocktailSearchRequest{
         private int page = 0;
         private int size = 10;
