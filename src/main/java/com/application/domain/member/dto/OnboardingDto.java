a0dc4b392dae88c2390ff47eeef622cdd351e37c
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-12-23 03:29:17 +0900

    [#61] docs: 프론트에서 보내줘야 하는 정보 구체적으로 문서화

--- src/main/java/com/application/domain/member/dto/OnboardingDto.java
+++ src/main/java/com/application/domain/member/dto/OnboardingDto.java
@@ -1,5 +1,6 @@
 package com.application.domain.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,11 +9,32 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Schema(description = "온보딩 정보 요청 DTO")
 public class OnboardingDto {
 
+    @Schema(
+            description = "성별 정보",
+            example = "male",
+            allowableValues = {"male", "female", "none"},
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     @NotBlank(message = "성별 정보는 필수입니다.")
-    private String gender; // "male", "female", "none"
+    private String gender;
 
+    @Schema(
+            description = """
+                    연령대 정보
+                    - under_19: 19세 이하
+                    - 20_24: 20-24세
+                    - 25_29: 25-29세
+                    - 30_34: 30-34세
+                    - 35_39: 35-39세
+                    - 50_over: 50세 이상
+                    """,
+            example = "20_24",
+            allowableValues = {"under_19", "20_24", "25_29", "30_34", "35_39", "50_over"},
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     @NotBlank(message = "연령대 정보는 필수입니다.")
-    private String ageRange; // "under_19", "20_24", "25_29", "30_34", "35_39", "50_over"
+    private String ageRange;
 }
